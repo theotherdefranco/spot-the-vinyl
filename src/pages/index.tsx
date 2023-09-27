b33ff942978786c4d3e344942b44c95e1e791b87
Nicholas DeFranco <nicholas.p.defranco@gmail.com>
2023-09-27 18:30:37 -0400

    Add artist images and update DB

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,30 @@ import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 import { Sign } from "crypto";
 
+const WelcomeWagon = () => {
+
+  const { user } = useUser();
+
+  if (!user) return null;
+
+  return (<div className="flex items-center text-xl gap-4">
+    <img src={user.imageUrl} alt='Profile image' className="rounded-lg" />
+    <p>Welcome {user.fullName}</p>
+
+  </div>
+  );
+}
+
 export default function Home() {
 
   const user = useUser();
+  //add user into DB
 
   const { data, isLoading } = api.artist.getAll.useQuery();
 
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) return <div className="flex justify-left">Loading...</div>
 
-  if (!data) return <div>Something went wrong!</div>
+  if (!data) return <div className="flex justify-left">Something went wrong!</div>
 
   return (
     <>
@@ -29,11 +44,13 @@ export default function Home() {
               <SignInButton />
             </div>
             )}
-            {!!user.isSignedIn && <SignOutButton />}
+            {!!user.isSignedIn && <WelcomeWagon />}
           </div>
-          <div className="flex flex-col">
+          <div className="grid grid-flow-col col-auto auto-cols-max">
             {[...data]?.map((artist) => (
-              <div key={artist.id} className="p-8 border-b border-slate-400">{artist.name}</div>))}
+              <div key={artist.id} className="p-8 text-center text-slate-200">
+                <img src={artist.image} alt='Artist Image' className="h-32 w-32 rounded-full" /> {artist.name}
+              </div>))}
           </div>
         </div>
       </main>
--- src/server/api/routers/artist.ts
+++ src/server/api/routers/artist.ts
@@ -1,9 +1,30 @@
+import { clerkClient, useUser } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/dist/types/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+
+    return {
+        id: user.id,
+        username: user.username,
+        profileImageUrl: user.imageUrl
+    };
+};
+
 export const artistRouter = createTRPCRouter({
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.db.artist.findMany();
+    getAll: publicProcedure.query(async ({ ctx }) => {
+        const arists = await ctx.db.artist.findMany({
+            take: 100,
+            where: {
+                UserArtists: {
+                    some: {
+                        userId: 'user_2VlBi7FNHK4fnFwrQOItPuxJduh'
+                    }
+                }
+            },
+        });
+        return arists;
     }),
 });
