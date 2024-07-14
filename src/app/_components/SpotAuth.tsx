022fb921fb0cc5a99922ddeebbb2164a1aa4031b
Nicholas DeFranco <nicholas.p.defranco@gmail.com>
2024-07-14 00:57:41 -0500

    fix prod redirect

--- src/app/_components/SpotAuth.tsx
+++ src/app/_components/SpotAuth.tsx
@@ -7,9 +7,9 @@ const spotifyTokenStorageID =
 let redirect = "";
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  redirect = "http://localhost:3000/";
+  redirect = "localhost:3000/";
 } else {
-  redirect = "https://spot-the-vinyl.vercel.app/";
+  redirect = "spot-the-vinyl.vercel.app/";
 }
 const clientId = process.env.NEXT_PUBLIC_VITE_SPOTIFY_CLIENT_ID;
 console.log(clientId);
